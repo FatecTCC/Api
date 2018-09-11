fe387f173d3a39dc7fe3957c74c6ae97f8728f44
vagrant <vagrant@api>
2018-09-11 15:53:34 +0000

    preparando ambiente API

---
+++ controller/connection.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+//ES6 Promises
+mongoose.Promise = global.Promise;
+
+module.exports = function(){
+	mongoose.connect("mongodb://10.0.0.12:27017/cliente", { useNewUrlParser: true });
+	mongoose.connection.once('open', function(){
+		console.log("Connection has beem made, now make fireworks!");
+	}).on('error', function(error){
+		console.log("Connection error: ", error);
+	});
+}
+
